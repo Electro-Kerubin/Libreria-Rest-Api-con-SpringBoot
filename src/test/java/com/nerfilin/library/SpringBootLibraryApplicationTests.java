3fe7ec2e5df1e027f8b101fbdaba2b8bb540a88f
Electro-kerubin <rbcirano.90@gmail.com>
2023-01-28 16:49:58 -0300

    primer commit

---
+++ src/test/java/com/nerfilin/library/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerfilin.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
